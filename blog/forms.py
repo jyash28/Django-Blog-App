f456140ba0cdc9010b0f859983f828c19fe63367
jyash28 <jyash548@gmail.com>
2021-03-16 22:13:57 +0530

    update

--- blog/forms.py
+++ blog/forms.py
@@ -8,4 +8,4 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','first_name','last_name','email']        
-        labels = {'first_name':'First Name','last_name': 'Last Name','email':'Email'}           
+        labels = {'first_name':'First Name','last_name': 'Last Name','email':'Email'}                   

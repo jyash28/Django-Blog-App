5faf90cdf6da3b7ef22bd8cf79b4b4d44eab02d8
jyash28 <jyash548@gmail.com>
2021-03-24 23:53:24 +0530

    update'

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,HttpResponseRedirect
 from .forms import SignUpForm,LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from .models import Post
 
 # home
 def home(request):
-    return render(request,'blog/home.html')
+    posts=Post.objects.all()
+    return render(request,'blog/home.html',{'posts':posts})
 
 # about
 def about(request):
@@ -15,19 +19,47 @@ def contact(request):
 
 # dashboard         
 def dashboard(request):
-    return render(request,'blog/dashboard.html')
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        return render(request,'blog/dashboard.html',{'posts':posts})
+    else:
+        return HttpResponseRedirect("/login/")
+
+    
 
 # Logout
 def user_logout(request):
+    logout(request)
     return HttpResponseRedirect('/')
 
 # Signup
 def user_signup(request):
-    form = SignUpForm()
-    return render(request,'blog/signup.html',{'form':form})
+    if request.method == "POST":
+        form= SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,"Congratulation !! You have become an Author")
+            form.save()
+    else:
+        form = SignUpForm()
+    return render(request,'blog/signup.html',{'form':form})                             
+    
 
 # Login
 def user_login(request):
-    form =LoginForm()
-    return render(request,'blog/login.html',{'form':form})
-
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginForm(request=request,data=request.POST)
+            if form.is_valid():
+                uname=form.cleaned_data["username"]
+                upass=form.cleaned_data["password"]
+                user=authenticate(username=uname,password=upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,"Logged in successfully!!!")
+                    return HttpResponseRedirect("/dashboard/")
+        else:
+            form =LoginForm()
+        return render(request,'blog/login.html',{'form':form})
+    else:
+        return HttpResponseRedirect("/dashboard/")
+    
